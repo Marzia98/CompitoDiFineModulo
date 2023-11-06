644214900c6a8df3fba8fb69afbd6aacffc2ec3b
Marzia98 <145680563+Marzia98@users.noreply.github.com>
2023-11-06 14:33:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,186 @@
+/*
+PARTE 1: 
+Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
+Il tuo compito è creare una funzione che cercherà per posizione lavorativa E posizione geografica. Questi due valori verranno passati come parametri
+Ti abbiamo fornito un array chiamato "jobs" in fondo al file, NON modificarlo in alcun modo.
+L'algoritmo che devi realizzare cercherà SIA per posizione lavorativa che per posizione geografica.
+Prendi queste tre inserzioni ad esempio:
+
+      job1:  location: "NY, US",     title: "java dev"
+      job2:  location: "Genoa, IT"   title: "web dev"
+      job3:  location: "US"      title: "dev"
+
+Cercando contemporaneamente come posizione lavorativa "dev" e posizione geografica "US", dovresti ottenere come risultato solamente job1 e job3,
+in quanto job2 non soddisfa la condizione posta sulla posizione geografica.
+
+REQUISITI:
+- il tuo algoritmo deve tornare i risultati nella seguente forma:
+{
+  result: [], <-- inserisci qui le inserzioni che rispecchiano la posizione lavorativa e la posizione geografica richiesta
+  count: 0 <-- inserisci qui il numero totale delle inserzioni trovate
+}
+
+- la tua ricerca deve essere "case insensitive" (non deve essere influenzata da lettere maiuscole o minuscole nelle parole cercate). Questo e' possibile trasformando tutto in lettere minuscole con .toLowerCase()
+
+
+PARTE 2: 
+Nella pagina HTML, inserisci 2 input di tipo testo (uno per la location e uno per il titolo lavorativo, ricordati di diversificarli con un id) e un bottone con valore “cerca”
+
+Al click del bottone, il codice deve raccogliere i valori dei due input e darli in pasto alla funzione che hai creato nella parte 1. 
+
+Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati sulla pagina: 
+    Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
+    Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
+    SUGGERIMENTO: ti servira’ un ciclo for!
+
+*/
+
+// NON MODIFICARE QUESTO ARRAY!
+const jobs = [
+  { title: "Marketing Intern", location: "US, NY, New York" },
+  {
+    title: "Customer Service - Cloud Video Production",
+    location: "NZ, Auckland",
+  },
+  {
+    title: "Commissioning Machinery Assistant (CMA)",
+    location: "US, IA, Wever",
+  },
+  {
+    title: "Account Executive - Washington DC",
+    location: "US, DC, Washington",
+  },
+  { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
+  { title: "Accounting Clerk", location: "US, MD," },
+  { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
+  {
+    title: "Lead Guest Service Specialist",
+    location: "US, CA, San Francisco",
+  },
+  { title: "HP BSM SME", location: "US, FL, Pensacola" },
+  {
+    title: "Customer Service Associate - Part Time",
+    location: "US, AZ, Phoenix",
+  },
+  {
+    title: "ASP.net Developer Job opportunity at United States,New Jersey",
+    location: "US, NJ, Jersey City",
+  },
+  {
+    title: "Talent Sourcer (6 months fixed-term contract)",
+    location: "GB, LND, London",
+  },
+  {
+    title: "Applications Developer, Digital",
+    location: "US, CT, Stamford",
+  },
+  { title: "Installers", location: "US, FL, Orlando" },
+  { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
+  {
+    title: "VP of Sales - Vault Dragon",
+    location: "SG, 01, Singapore",
+  },
+  { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
+  {
+    title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
+    location: "GB, SOS, Southend-on-Sea",
+  },
+  { title: "Visual Designer", location: "US, NY, New York" },
+  {
+    title: "Process Controls Engineer - DCS PLC MS Office - PA",
+    location: "US, PA, USA Northeast",
+  },
+  { title: "Marketing Assistant", location: "US, TX, Austin" },
+  { title: "Front End Developer", location: "NZ, N, Auckland" },
+  { title: "Engagement Manager", location: "AE," },
+  {
+    title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
+    location: "US, CA, Carlsbad",
+  },
+  { title: "Customer Service", location: "GB, LND, London" },
+  { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
+  { title: "Marketing Exec", location: "SG," },
+  {
+    title: "HAAD/DHA Licensed Doctors Opening in UAE",
+    location: "AE, AZ, Abudhabi",
+  },
+  {
+    title: "Talent Management Process Manager",
+    location: "US, MO, St. Louis",
+  },
+  { title: "Customer Service Associate", location: "CA, ON, Toronto" },
+  {
+    title: "Customer Service Technical Specialist",
+    location: "US, MA, Waltham",
+  },
+  { title: "Software Applications Specialist", location: "US, KS," },
+  { title: "Craftsman Associate", location: "US, WA, Everett" },
+  { title: "Completion Engineer", location: "US, CA, San Ramon" },
+  { title: "I Want To Work At Karmarama", location: "GB, LND," },
+  {
+    title: "English Teacher Abroad",
+    location: "US, NY, Saint Bonaventure",
+  },
+];
+
+// Creo una funzione che abbia come parametri PositionJob e GeographicLocation
+// al suo interno inserisco .toLowerCase per trasformare  tutto in lettere minuscole 
+
+const stampaLavori = function(PositionJob, GeographicLocation) {
+
+ // creo un array di output vuoto ed un count
+
+ let result = [];
+ let Count = 0;
+
+//  importante creare il ciclo for 
+//il cilo è composto da un inizio, una fine e dalla direzione
+// parte da zero, l'esecuzione che voglio in questo caso è la lunghezza
+for (let i = 0; i < jobs.length; i++) {
+  if (
+    jobs[i].title.toLowerCase().includes(PositionJob.toLowerCase()) &&
+    jobs[i].location.toLowerCase().includes(GeographicLocation.toLowerCase())
+  ) {
+    result.push(jobs[i]);
+    Count++;
+  }
+} 
+
+return {
+  result: result,
+  count: Count
+};
+};
+   //ottengo il pulsante di ricerca 
+const searchButton = document.getElementById("searchButton");
+
+// Aggiungo un Listener che mi serve a gestire il click sul pulsante di ricerca
+// Quando un utente clicca il pulsante, il codice viene eseguito
+// utilizzo il getElementById per la posizione lavorativa e la posizione geografica
+searchButton.addEventListener("click", function () {
+  const PositionJob = document.getElementById("jobTitleInput").value;
+  const GeographicLocation = document.getElementById("jobLocationInput").value;
+
+
+//Il codice cancella i risultati precedenti nella lista e me ne fa ottenere di nuovi
+  const searchResult = stampaLavori(PositionJob, GeographicLocation);
+  const resultList = document.getElementById("resultList");
+  resultList.innerHTML = ""; 
+
+// con questo ciclo for collegato con il resultList nella pagina dell'html
+//quando inserisco i miei criteri di ricerca mi crea tramite il sito web una lista di risultati.
+  for (let i = 0; i < searchResult.result.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = `${searchResult.result[i].title} - ${searchResult.result[i].location}`;
+    resultList.appendChild(li);
+  }
+//Grazie a questo codice riesco a visualizzare il numero di risultati nell'html, che collego tramite un id che ho creato "countContainer" all'interno di un paragrafo
+  const countContainer = document.getElementById("countContainer");
+  countContainer.textContent = `Risultati trovati: ${searchResult.count}`;
+});
+
+
+
+
+
+
